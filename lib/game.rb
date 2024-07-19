45c0a10bb4ae16eb2694fd5d4fb20f2a1251b76f
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-07-19 13:58:32 +0200

    Work on game class

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,17 @@ require_relative "file_handler"
 
 # Class with game logic
 class Game
-  def initialize(guess_amount)
+  def initialize(guess_amount, path)
     @guess_amount = guess_amount
+    @file_handler = FileHandler.new(path)
+    @wordlist = file_handler.wordlist
+    @chosen_word = choose_word
+    @guessed_letters = Array.new
   end
 
   private
 
+  def choose_word()
+    wordlist.sample
+  end
 end
